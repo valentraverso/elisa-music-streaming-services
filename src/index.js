33ca0e86740acfb23d350370db38b2197938fe40
Valentino Traverso <vt20032003@live.com.ar>
2023-04-14 10:42:16 +0200

    feat(add middlewares): cors, helmet, express.json

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
 
 
 const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors);
+app.use(helmet());
+
+// Conect DB
