98cb761db2d21c10a77df3e35df2548712303b1d
Valentino Traverso <vt20032003@live.com.ar>
2023-04-14 11:13:51 +0200

    feat: connection db with mongoose

--- src/utils/connectDB.js
+++ src/utils/connectDB.js
@@ -1,5 +1,16 @@
 const mongoose = require('mongoose');
 
-function connectDB(app, PORT, DB){
-    
+function connectDB(app, PORT, DB) {
+    mongoose.connect(DB)
+        .then(() => {
+                app.listen(PORT, () => {
+                    console.log(`
+                    ---- ELISA SERVER -----
+                    PORT: ${PORT}
+                    `)
+                })
+        })
+        .catch(err => console.log(err))
 }
+
+module.exports = connectDB;
