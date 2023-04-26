ed70caf8fecdced383a8d9352bdf8180f49db9c7
Albert Gomis <albert.gomis.20@gmail.com>
2023-04-26 16:50:39 +0200

    add: user model and user controller

--- src/models/index.js
+++ src/models/index.js
@@ -1,6 +1,8 @@
 const albumModel = require("./albums.models")
+const UserModel = require("./users.model.js")
 
 
 module.exports = {
-    albumModel
+    albumModel,
+    UserModel
 }
---
+++ src/models/users.model.js
@@ -0,0 +1,52 @@
+const {Schema, model, Types} = require("mongoose")
+
+const UserSchema = new Schema({
+    name:{
+        type: String,
+        required: [true, 'The name is requerid']
+    },
+    email:{
+        type: String,
+        required: [true, 'The email is requerid']
+    },
+    picture:{
+        type: String
+    },
+    follows:[{
+        type: Types.ObjectId,
+        ref: "follows"
+    }],
+    followers:[{
+        type: Types.ObjectId,
+        ref: "followers"
+    }],
+    songs:[{
+        type: Types.ObjectId,
+        ref: "songs"
+    }],
+    playlists:[{
+        type: Types.ObjectId,
+        ref: "playlists"
+    }],
+    albums:[{
+        type: Types.ObjectId,
+        ref: "albums"
+    }],
+    genres:[{
+        type: Types.ObjectId,
+        ref: "genres"
+    }],
+    artists:[{
+        type: Types.ObjectId,
+        ref: "artists"
+    }],
+    role:{
+        type: String
+    }
+    
+
+})
+
+const UserModel = model("User", UserSchema)
+
+module.exports = UserModel
