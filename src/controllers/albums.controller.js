6972838f099ee9f553497b35cd5438620088094f
Ynohe Sandoval <ynotimelesss@gmail.com>
2023-04-26 10:29:03 +0100

    add: albums controller

---
+++ src/controllers/albums.controller.js
@@ -0,0 +1,28 @@
+const {albumModel} = require("../models");
+
+
+const albumController = {
+    getAllAlbum: async (req, res)=>{
+        try{
+            const album = await albumModel.find();
+
+            if(!album){
+                res.status(404).send({
+                    status: false,
+                    msg: "We coundn't find albums",
+                })
+            }
+
+            res.status(200).send({
+                status: true,
+                msg: "We found albums",
+                data: album
+            })
+        }catch(error){
+            res.status(500).send({
+                status: false,
+                msg: error
+            })
+        }
+    },
+}
