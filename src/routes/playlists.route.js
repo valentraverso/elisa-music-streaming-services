86b1cd35e8defe61dccf74466c8bd9a8d121b2f4
Ynohe Sandoval <ynotimelesss@gmail.com>
2023-05-04 16:29:07 +0100

    feat: add playlist controller and getByTitle in playlist route

--- src/routes/playlists.route.js
+++ src/routes/playlists.route.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 const { playlistController } = require("../controllers");
 
-const { getAllPlaylist, createPlaylist, getPlaylistById, updatePlaylist, deletePlaylist } = playlistController;
+const { getAllPlaylist, createPlaylist, getPlaylistById, updatePlaylist, deletePlaylist, getByTitle } = playlistController;
 
 router
     .get("/all", getAllPlaylist)
     .get("/id/:id", getPlaylistById)
+    .get("/title", getByTitle)
     .post("/create", createPlaylist)
     .patch("/update/id", updatePlaylist)
     .delete("/delete/:id", deletePlaylist)
