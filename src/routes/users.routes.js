6ab850bf951b2f7fe17a37e8f229b73a6520ef97
Valentino Traverso <vt20032003@live.com.ar>
2023-05-04 19:07:44 +0200

    feat: order methods

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -5,12 +5,13 @@ const { userController } = require("../controllers");
 const {signUp, getBySub, getById, updateArray, updateBasic, deleteUser, getByName} = userController;
 
 router
-.post("/create", signUp)
 .get("/sub/:userSub", getBySub)
 .get("/id/:userId", getById)
+.get("/name/:userName", getByName)
 .patch("/update/array/:userId", updateArray)
 .patch("/update/:userId", updateBasic)
 .patch("/delete/:userId", deleteUser)
-.get("/name/:userName", getByName)
+.post("/create", signUp)
+
 
 module.exports = router;
